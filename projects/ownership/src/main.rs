09459bda249e9e65b74b4d4fbbea1a50439f0cdf
Joel Tony <joeltony72@gmail.com>
2023-07-26 19:18:58 +0530

    Fix returning a reference to the stack https://rust-book.cs.brown.edu/ch04-03-fixing-ownership-errors.html#fixing-an-unsafe-program-returning-a-reference-to-the-stack

--- projects/ownership/src/main.rs
+++ projects/ownership/src/main.rs
@@ -1,11 +1,7 @@
+fn return_a_string(output: &mut String) {
+    output.replace_range(.., "Hello world");
+}
 fn main() {
-    let mut x: Box<i32> = Box::new(1); // Create a box
-    let _a: i32 = *x; // Dereference the box
-    *x += 1; // Increase the value inside the box to 2
-
-    let r1: &Box<i32> = &x; // Point to x on the stack
-    let _b: i32 = **r1; // Dereference the box twice to reach the data on the heap
-
-    let r2: &i32 = &*x; // Directly point to the address of x on the heap
-    let _c: i32 = *r2; // Dereference x on the heap
+    let mut fn_output: String = String::from("");
+    return_a_string(&mut fn_output); // Similar to how file descriptors are recieved in C
 }
